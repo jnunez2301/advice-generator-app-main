ae886df0621cffefabddd54ca1fb5575246df555
Roxc2301 <131661824+Roxc2301@users.noreply.github.com>
2023-08-03 14:45:28 +0200

    added new features
    -When loading it will show a kamoji saying cool things
    
    -Whenever the page loads it will give a random advice instead of a pre written one
    
    -When a new advice is loaded it will make sure to never repeat the advice with the last one.

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 const dice = document.querySelector('.dice');
 const randomQuote = document.querySelector('#randomQuote');
 const theNumbersMason = document.querySelector('#theNumbersMason');
+let lastAdviceId = null;
 
-dice.addEventListener('click', function handleClick() {
+// Load a random quote when the page is loaded
+window.addEventListener('load', function () {
+  randomQuote.innerHTML = `Patience is key <br />♡( ◡‿◡ )`;
+  theNumbersMason.innerHTML = '000';
+
+  fetchAdvice();
+});
+
+function fetchAdvice() {
   fetch('https://api.adviceslip.com/advice')
     .then(response => response.json())
     .then(data => {
       const advice = data.slip.advice;
-      randomQuote.innerHTML = '"' + advice + '"';
       const adviceId = data.slip.id;
-      theNumbersMason.innerHTML = adviceId;
-
+      
+      // Check if the new advice ID is the same as the last one
+      if (adviceId === lastAdviceId) {
+        // Fetch again to get a new quote
+        fetchAdvice();
+      } else {
+        randomQuote.innerHTML = '"' + advice + '"';
+        theNumbersMason.innerHTML = adviceId;
+        // Update the last advice ID
+        lastAdviceId = adviceId;
+      }
     })
     .catch(error => {
       console.log('An error occurred:', error);
     });
+}
+
+dice.addEventListener('click', function handleClick() {
+  theNumbersMason.innerHTML = '000';
+  randomQuote.innerHTML = `Looking for more deep thoughts <br />(；⌣̀_⌣́)...`;
+  theNumbersMason.innerHTML = ''; // Clear previous advice ID if any
+
+  fetchAdvice();
 });
